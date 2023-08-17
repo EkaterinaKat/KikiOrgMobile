9fda656b1874fa0c77572603b0710cbb65635266
EkaterinaKat <katyshevtseva_ekaterina@mail.ru>
2023-08-17 21:11:22 +0500

    Группировка задач по времени суток

--- app/src/main/java/com/katyshevtseva/kikiorgmobile/view/MainTaskListFragment.java
+++ app/src/main/java/com/katyshevtseva/kikiorgmobile/view/MainTaskListFragment.java
@@ -16,12 +16,14 @@ import androidx.fragment.app.Fragment;
 
 import com.example.kikiorgmobile.R;
 import com.katyshevtseva.kikiorgmobile.core.Service;
+import com.katyshevtseva.kikiorgmobile.core.enums.TimeOfDay;
 import com.katyshevtseva.kikiorgmobile.core.model.Task;
 import com.katyshevtseva.kikiorgmobile.utils.knobs.NoArgKnob;
 import com.katyshevtseva.kikiorgmobile.view.utils.ViewUtils;
 
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import lombok.RequiredArgsConstructor;
 
@@ -73,28 +75,50 @@ public class MainTaskListFragment extends Fragment {
 
     private void showTasks() {
         taskBox.removeAllViews();
-        for (Task task : tasks) {
-            LinearLayout linearLayout = new LinearLayout(getContext());
-            TextView textView = new TextView(getContext());
 
+        for (TimeOfDay timeOfDay : TimeOfDay.values()) {
+
+            LinearLayout linearLayout = new LinearLayout(getContext());
+            linearLayout.setOrientation(LinearLayout.VERTICAL);
+            linearLayout.setBackground(ViewUtils.getBackground2(timeOfDay, getContext()));
+            linearLayout.setMinimumWidth(650);
             linearLayout.setGravity(Gravity.CENTER);
-            linearLayout.setBackground(ViewUtils.getBackground2(task.getTimeOfDay(), getContext()));
-//            linearLayout.setBackground(ViewUtils.getBackground(task.getUrgency(), getContext()));
-            linearLayout.setMinimumWidth(500);
-            linearLayout.setOnClickListener(view -> taskClickListener(task));
 
             LayoutParams params = new LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
             params.setMargins(15, 15, 15, 15);
             linearLayout.setLayoutParams(params);
 
-            textView.setText(task.getTitle());
-            textView.setSingleLine(false);
-            textView.setPadding(15, 15, 15, 15);
-            textView.setTextSize(16);
-
-            linearLayout.addView(textView);
+            for (Task task : getTasksByTimeOfDay(timeOfDay)) {
+                linearLayout.addView(getTaskView(task));
+            }
             taskBox.addView(linearLayout);
         }
+    }
+
+    private List<Task> getTasksByTimeOfDay(TimeOfDay timeOfDay) {
+        return tasks.stream().filter(task -> task.getTimeOfDay() == timeOfDay).collect(Collectors.toList());
+    }
+
+    private View getTaskView(Task task) {
+        LinearLayout linearLayout = new LinearLayout(getContext());
+        TextView textView = new TextView(getContext());
+
+        linearLayout.setGravity(Gravity.CENTER);
+        linearLayout.setBackground(ViewUtils.getBackground(task.getUrgency(), getContext()));
+        linearLayout.setMinimumWidth(500);
+        linearLayout.setOnClickListener(view -> taskClickListener(task));
+
+        LayoutParams params = new LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
+        params.setMargins(15, 15, 15, 15);
+        linearLayout.setLayoutParams(params);
+
+        textView.setText(task.getTitle());
+        textView.setSingleLine(false);
+        textView.setPadding(15, 15, 15, 15);
+        textView.setTextSize(16);
+
+        linearLayout.addView(textView);
+        return linearLayout;
     }
 
     private void taskClickListener(Task task) {
